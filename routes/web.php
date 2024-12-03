5b1ac404ae491094692def94e85b34c18d163558
hardikwps <hardiksharmawps@gmail.com>
2024-12-03 16:00:59 +0530

    check

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+//View Route
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Basic Route
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+//Named Route
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+//Route GroupsT
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+require __DIR__.'/admin-auth.php';
+require __DIR__.'/teacher-auth.php';
+
